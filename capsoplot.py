95da966e4325ddff8ec6f389cbf54d22714a435c
m3cic <kingarpharazon@gmail.com>
2013-10-31 14:50:25 -0600

    Update docstring
    - Function: plot_time_series

--- capsoplot.py
+++ capsoplot.py
@@ -34,11 +34,11 @@ def plot_time_series(file_name, tmin=-1, tmax=-1):
     Plot the time series of a CaPso results file.
 
     Args:
-        file_name (str)   -- the text file containing the results.
+        file_name (str): the text file containing the results.
 
-    Keyword arguments:
-        tmin=-1 (int)     -- the minimal endpoint of the time interval to plot.
-        tmax=-1 (int)     -- the maximum endpoint of the time interval to plot.
+    Kwargs:
+        tmin (int): the minimal endpoint of the time interval to plot.
+        tmax (int): the maximum endpoint of the time interval to plot.
 
     """
     fig = figure(1)
