408ef236d445f7e5a0d5254675ca83dd878f96e8
kingarpharazon@numenor <mariomartinezmolina@live.com>
2013-12-16 20:32:16 -0600

    Set default sizes for the figures
    Make this change for plot_time_series_normalized() and
    plot_mf_coupled()

--- capsoplot.py
+++ capsoplot.py
@@ -147,7 +147,7 @@ def plot_time_series_normalized(file_name, width=512, height=256, tmin=-1,
     """
     size = width * height
 
-    figure(1)
+    figure(1, (9, 8))
 
     _set_font()
 
@@ -753,7 +753,7 @@ def plot_mf_coupled(N=100, psi0=1, phi0=0.01, alpha=0.1, ey=1, ry=1, ez=1,
         psi[t] = psi_dy + (1 - psi_dy) * (1 - (1 - py) ** number_of_events)
 
     # Setup the plot
-    figure(1)
+    figure(1, (9, 8))
 
     _set_font()
 
